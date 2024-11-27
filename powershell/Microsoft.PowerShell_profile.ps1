ec97d55012b530d3b20749639c876373d99dea93
Anonymous <dennischris56@gmail.com>
2024-11-27 08:10:22 +0000

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,10 @@ function Get-Uptime {
     $uptime = New-TimeSpan -Start (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
     return "{0} days, {1} hours, {2} minutes, {3} seconds" -f $uptime.Days, $uptime.Hours, $uptime.Minutes, $uptime.Seconds
 }
+
+function Wotaku {
+    $MediaType = Read-Host "Do you want to watch (a)nime or read (m)anga?"
+    if ($MediaType -eq 'a' -or $MediaType -eq 'anime') { & "sh" "ani-cli" $args }
+    elseif ($MediaType -eq 'm' -or $MediaType -eq 'manga') { & "mangal" $args }
+    else { Write-Host "Invalid choice. Please choose 'anime' or 'manga'." -ForegroundColor Red }
+}
