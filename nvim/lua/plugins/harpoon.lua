b64c8f4aca11b05b91978f1fdc8a1cb932f4d470
Anonymous <dennischris56@gmail.com>
2024-11-27 10:26:14 +0000

    Create harpoon.lua

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,8 @@
+return {
+    "ThePrimeagen/harpoon",
+    lazy = true,
+    cmd = "Harpoon", -- This will lazy-load Harpoon only when the `:Harpoon` command is called
+    config = function()
+        -- Configuration code goes here
+    end
+}
