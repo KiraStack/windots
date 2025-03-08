4e88ddf21d4cfe343aa62a8409956e640f5fa1ff
Anonymous <dennischris56@gmail.com>
2025-03-08 19:02:10 +0000

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,19 @@
-# Initialize Starship prompt  
-# Invoke-Expression (&starship init powershell)  
+# Initialize Starship prompt
+# Invoke-Expression (&starship init powershell) 
 
-Enable-PowerType # Enhanced autocomplete  
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView # Enable predictions  
+Enable-PowerType # Enhanced autocomplete
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView # Enable predictions
 
-# Clear command history  
-function Remove-History {  
-    Remove-Item (Get-PSReadlineOption).HistorySavePath -Force  
-}  
+# Clear command history
+function Remove-History {
+    Remove-Item (Get-PSReadlineOption).HistorySavePath -Force
+}
 
-# Show system uptime  
-function Get-Uptime {  
-    $uptime = New-TimeSpan -Start (Get-CimInstance Win32_OperatingSystem).LastBootUpTime  
-    return "$($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m $($uptime.Seconds)s"  
+# Show system uptime
+function Get-Uptime {
+    $u = New-TimeSpan -Start (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+    Write-Host "Uptime: " -NoNewline -ForegroundColor Green
+    Write-Host "$("{0:D2}" -f $u.Days):$("{0:D2}" -f $u.Hours):$("{0:D2}" -f $u.Minutes):$("{0:D2}" -f $u.Seconds)"
 }
 
 function prompt {
