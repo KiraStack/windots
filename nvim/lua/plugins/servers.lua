25f76b994678fd187789ca4a38129a258c239332
Anonymous <dennischris56@gmail.com>
2025-04-16 01:31:21 +0100

    feat: added luau support

--- nvim/lua/plugins/servers.lua
+++ nvim/lua/plugins/servers.lua
@@ -1,43 +1,48 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        event = "VeryLazy", -- Load after full startup
-        dependencies = {"williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig"},
-        config = function()
-            local servers = {"pyright", "ts_ls"}
+	{
+		"williamboman/mason.nvim",
+		event = "VeryLazy", -- Load after full startup
+		dependencies = { "neovim/nvim-lspconfig", "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			local mason = require("mason")
+			local mason_lsp = require("mason-lspconfig")
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local servers = { "ts_ls", "pyright" }
 
-            require("mason").setup()
-            require("mason-lspconfig").setup({ensure_installed = servers})
+			mason.setup()
+			mason_lsp.setup({ ensure_installed = servers })
+		end
+	},
+	{
+		"lopi-py/luau-lsp.nvim",
+		event = "VeryLazy", -- Load after full startup
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local function rojo_project()
+				return vim.fs.root(0, function(name)
+					return name:match ".+%.project%.json$"
+				end)
+			end
 
-            local on_attach = function(client, bufnr)
-                -- Enable diagnostics
-                vim.diagnostic.config(
-                    {
-                        virtual_text = true,
-                        signs = true,
-                        underline = true,
-                        update_in_insert = false,
-                        severity_sort = true
-                    }
-                )
-            end
-
-            for _, server in ipairs(servers) do
-                require("lspconfig")[server].setup(
-                    {
-                        on_attach = on_attach,
-                        capabilities = require("cmp_nvim_lsp").default_capabilities()
-                    }
-                )
-            end
-        end
-    },
+			if rojo_project() then
+				vim.filetype.add {
+					extension = {
+						lua = function(path)
+							return path:match "%.nvim%.lua$" and "lua" or "luau"
+						end,
+					},
+				}
+			end
+		end
+	},
     {
         "stevearc/conform.nvim",
         event = "BufReadPost", -- Load when a buffer is read
         opts = {
             formatters_by_ft = {
                 python = {"black"}, -- Black for Python
+				lua = {"stylua"}, luau = {"stylua"},
                 javascript = {"prettierd"} -- Prettier for JavaScript
             }
         },
