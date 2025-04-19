95b7565c204fc6223d40083921949fd49b4a694e
Anonymous <dennischris56@gmail.com>
2025-04-19 14:57:23 +0100

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,60 +1,68 @@
+-- Modules
 local wezterm = require("wezterm")
+
+-- Service
 local config = wezterm.config_builder()
 
--- Function to determine the color scheme based on appearance
-function scheme_for_appearance(appearance)
-	return (appearance:find("Dark") and "OneDark (base16)") or "One Light (base16)"
+-- References
+local themes = {
+	Standard = "Tokyo Night",
+	Storm = "Tokyo Night Storm",
+	Moon = "Tokyo Night Moon",
+	Day = "Tokyo Night Day",
+	Night = "Tokyo Night Night",
+}
+
+-- Settings
+local override_theme = nil
+
+-- Auxiliary
+local function resolve_scheme()
+	if override_theme and themes[override_theme] then
+		return themes[override_theme]
+	end
+	local appearance = wezterm.gui.get_appearance()
+	return appearance:find("Dark") and themes.Storm or themes.Day
 end
 
--- Set color scheme dynamically based on system appearance
-local appearance = wezterm.gui.get_appearance()
-config.color_scheme = scheme_for_appearance(appearance)
+-- Functions
+local function bind_tabs(keys, mods)
+	local func = wezterm.action.ActivateTab
+	for i = 1, 9 do
+		keys[#keys + 1] = {key = tostring(i), mods = mods, action = func(i - 1)}
+	end
+end
 
--- Font settings
+
+-- Variables
+config.color_scheme = resolve_scheme()
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16
 config.bold_brightens_ansi_colors = true
-
--- Cursor and window settings
 config.default_cursor_style = "SteadyBar"
 config.default_prog = { "powershell.exe", "-nologo" }
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-
--- Auto-reload and keybindings
 config.automatically_reload_config = true
 config.disable_default_key_bindings = true
 
--- Leader key settings
+-- Main functions
 config.leader = {
-	key = "Space",
-	mods = "CTRL|SHIFT",
-	timeout_milliseconds = 1000,
+  key = "Space",
+  mods = "CTRL|SHIFT",
+  timeout_milliseconds = 1000,
 }
 
--- Custom key bindings
 config.keys = {
-	-- Pane and tab management
-	{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") }, -- New tab
-	{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane({ confirm = true }) }, -- Close pane with confirmation
-
-	-- Window management
-	{ key = "n", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen }, -- Toggle fullscreen
-	{ key = "d", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) }, -- Horizontal split
-	{ key = "e", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) }, -- Vertical split
-
-	-- Clipboard management
-	{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") }, -- Copy to clipboard
-	{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") }, -- Paste from clipboard
+  { key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+  { key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+  { key = "n", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
+  { key = "d", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "e", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+  { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
 }
 
--- Tab activation (CTRL+ALT+1-8)
-for i = 1, 8 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "CTRL|ALT",
-		action = wezterm.action.ActivateTab(i - 1),
-	})
-end
+bind_tabs(config.keys, "CTRL|ALT")
 
 return config
