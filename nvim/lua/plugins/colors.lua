4f5e0bd10eb7805e6f5869b6a8318f8d5d9876ef
Kira <dennischris56@gmail.com>
2025-06-26 12:34:37 +0100

    fix(nvim): revert to fallback theme

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,28 +1,42 @@
 return {
-	{
-		"kyomi",
-		dir = vim.fn.stdpath("config") .. "/", -- Path to the theme directory
-		lazy = false, -- Load immediately
-		priority = 10000, -- Highest priority to load before everything else
-		config = function()
-			-- Load the theme
-			local theme = require("kyomi")
 
-			-- Apply custom options
-			theme.setup()
+	{
+
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Load the colorscheme
+			vim.cmd.colorscheme("tokyonight-storm")
 		end,
 	},
+
 	{
+
 		"zaldih/themery.nvim",
-		cmd = "Themery",
+		cmd = "Themery", -- Load when the :Themery command is run
 		keys = {
-			{ "<leader>ct", "<cmd>Themery<CR>", desc = "Toggle theme" },
+			-- Toggle theme with <leader>ct (C-t)
+			{ "<leader>ct", ":Themery<CR>", "Toggle theme" },
 		},
 		opts = {
+			-- Custom themes to be available for selection
 			themes = {
-				{ name = "Kyomi", colorscheme = "kyomi" },
+				{ name = "Night", colorscheme = "tokyonight-night" },
+				{ name = "Storm", colorscheme = "tokyonight-storm" },
+				{ name = "Day", colorscheme = "tokyonight-day" },
+				{ name = "Moon", colorscheme = "tokyonight-moon" },
+
+				-- Add more themes as needed...
 			},
-			livePreview = true,
+			livePreview = true, -- Enable live preview
 		},
+		config = function(_, opts)
+			-- Load themery
+			local themery = require("themery")
+
+			-- Apply custom options
+			themery.setup(opts)
+		end,
 	},
 }
