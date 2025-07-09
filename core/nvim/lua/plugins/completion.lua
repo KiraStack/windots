2124248f1f041e83e79a6f56aa1b2c0d5ce82edd
Kira <dennischris56@gmail.com>
2025-07-09 19:32:27 +0100

    chore: add symbols to nvim-cmp

--- core/nvim/lua/plugins/completion.lua
+++ core/nvim/lua/plugins/completion.lua
@@ -34,8 +34,114 @@ return {
       'saadparwaiz1/cmp_luasnip',
     },
     config = function()
-      -- [Previous nvim-cmp configuration remains unchanged]
-      -- ... rest of the config ...
+      -- Load required modules
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      -- Flag to toggle symbols
+      local show_symbols = true
+
+      -- Icons for nvim-cmp menu
+      local symbols = {
+        Text = '',
+        Function = '󰆧',
+        Method = 'ƒ',
+        Constructor = '',
+        Field = '󰜢',
+        Variable = '󰀫',
+        Class = '󰠱',
+        Interface = '',
+        Module = '',
+        Property = '󰜢',
+        Unit = '',
+        Value = '',
+        Enum = '',
+        Keyword = '',
+        Snippet = '',
+        Color = '',
+        File = '',
+        Reference = '',
+        Folder = '',
+        EnumMember = '',
+        Constant = '',
+        Struct = '',
+        Event = '',
+        Operator = '',
+        TypeParameter = ''
+      }
+
+      -- Highlight groups for selection
+      vim.api.nvim_set_hl(0, "CmpPmenuSel", { link = "PmenuSel" })
+
+      local options = {
+        -- Disable auto-selection of first item
+        preselect = cmp.PreselectMode.None,
+
+        -- Configure nvim-cmp behavior
+        completion = {
+          completeopt = 'menu,menuone,noselect',
+        },
+
+        -- Configure luasnip snippets
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        -- Configure nvim-cmp formatting
+        formatting = {
+          format = function(entry, item)
+            local icon = (show_symbols and symbols[item.kind]) or symbols['Text'] or ''
+            local kind = item.kind or ''
+
+            item.menu = kind
+            item.menu_hl_group = 'CmpItemKind' .. kind
+            item.kind = icon
+
+            if #item.abbr > 30 then
+              item.abbr = string.sub(item.abbr, 1, 30) .. '…'
+            end
+
+            return item
+          end,
+        },
+
+        -- Configure nvim-cmp window
+        window = {
+          completion = {
+            scrollbar = false,
+            side_padding = 1,
+            winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:CmpPmenuSel,Search:None',
+            border = 'single',
+          },
+          documentation = {
+            border = 'single',
+            winhighlight = 'Normal:CmpDoc,FloatBorder:CmpDocBorder',
+          },
+        },
+
+        -- Configure key mappings
+        mapping = {
+          ['<Up>'] = cmp.mapping.select_prev_item(),
+          ['<Down>'] = cmp.mapping.select_next_item(),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+        },
+
+        -- Setup nvim-cmp sources
+        sources = {
+          { name = 'nvim_lsp' }, -- Language server
+          { name = 'luasnip' },  -- Snippets
+          { name = 'buffer' },   -- Text within current buffer
+          { name = 'path' },     -- File-system paths
+          { name = "codeium" }   -- AI-powered code completion
+        },
+      }
+
+      require('luasnip.loaders.from_vscode').lazy_load() -- Load snippets
+      cmp.setup(options)                                 -- Setup nvim-cmp_luasnip
     end,
   },
 
